81b36b14cf5160e3ee1d5399506a666a938efffc
Jacky Chan <cjacky624@gmail.com>
2019-02-20 08:54:21 -0500

    Initial commit

---
+++ 2-19Lab23Scaffold/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _2_19Lab23Scaffold.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string Name, string Email, string Password)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.Password = Password;
+        }
+
+        public User()
+        {
+
+        }
+    }
+
+    public class UserDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
